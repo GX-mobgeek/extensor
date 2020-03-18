371229e05f218a9c446ffa67ff6091c1b81aa78b
ferco0 <ferco0@live.com>
2020-03-18 06:17:17 -0300

    remove merge conflict marker

--- src/symbols.ts
+++ src/symbols.ts
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 const kExtensorAuthHandling: string = Symbol("extensorIoAuthHandling") as any;
-=======
->>>>>>> 0c4e978af7643641334f7ef58a1838334398df85
 const kSocketAuthStatus: string = Symbol("extensorSocketAuthStatus") as any;
 const kSocketAuthTimeout: string = Symbol("extensorSocketAuthTimeout") as any;
 
